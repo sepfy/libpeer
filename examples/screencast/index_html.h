20f440b4244ae05c278c13b04b309af1b2d5b41b
sepfy <sepfy95@gmail.com>
2022-01-13 00:10:57 +0000

    Modify screen recv example

--- examples/screencast/index_html.h
+++ examples/screencast/index_html.h
@@ -1,5 +1,5 @@
-#ifndef SCREEN_RECV_INDEX_HTML_H_
-#define SCREEN_RECV_INDEX_HTML_H_
+#ifndef SCREENCAST_INDEX_HTML_H_
+#define SCREENCAST_INDEX_HTML_H_
  
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,10 +8,9 @@ const char index_html[] = " \
 <!DOCTYPE html> \n \
 <html> \n \
   <head> \n \
-    <title>Screen Recv</title> \n \
+    <title>Screencast</title> \n \
   </head> \n \
   <body> \n \
-    <video style='display:block; margin: 0 auto;' id='localVideo'></video> \n \
     <script> \n \
       var pc = new RTCPeerConnection({ \n \
         iceServers: [{urls: 'stun:stun.l.google.com:19302'}] \n \
@@ -32,20 +31,12 @@ const char index_html[] = " \
         xhttp.setRequestHeader('Content-Type', 'plain/text'); \n \
         xhttp.send(btoa(JSON.stringify({'type': 'offer', 'sdp': sdp}))); \n \
       } \n \
-      pc.ontrack = function (event) { \n \
-        var el = document.getElementById('remoteVideos'); \n \
-        el.srcObject = event.streams[0]; \n \
-        el.autoplay = true; \n \
-        el.controls = true; \n \
-        el.muted = true; \n \
-      }; \n \
       pc.oniceconnectionstatechange = e => log(pc.iceConnectionState); \n \
       pc.onicecandidate = event => { \n \
         if(event.candidate === null) sendOfferToChannel(pc.localDescription.sdp) \n \
       }; \n \
       navigator.mediaDevices.getDisplayMedia({ video: true, audio: false }) \n \
         .then(stream => { \n \
-          document.getElementById('localVideo').srcObject = stream \n \
           stream.getTracks().forEach(track => pc.addTrack(track, stream)); \n \
             pc.createOffer().then(d => pc.setLocalDescription(d)).catch(log) \n \
           }).catch(log) \n \
@@ -53,4 +44,4 @@ const char index_html[] = " \
   </body> \n \
 </html>";
 
-#endif // SCREEN_RECV_INDEX_HTML_H_
+#endif // SCREENCAST_INDEX_HTML_H_
---
+++ examples/screencast/main.c
@@ -0,0 +1,142 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <gst/gst.h>
+
+#include "index_html.h"
+#include "signaling.h"
+#include "peer_connection.h"
+
+const char VIDEO_SRC_PIPELINE[] = "appsrc name=video-app-src format=time ! application/x-rtp,clock-rate=90000,encoding-name=H264 ! rtph264depay ! h264parse config-interval=-1 ! v4l2h264dec ! kmssink";
+
+typedef struct Screencast {
+
+  GstElement *video_src_pipeline;
+
+  GstElement *video_src;
+
+  GCond cond;
+  GMutex mutex;
+
+  PeerConnection *pc;
+  Signaling *signaling;
+
+} Screencast;
+
+Screencast g_screencast = {0};
+
+static void on_iceconnectionstatechange(IceConnectionState state, void *data) {
+
+  if(state == FAILED) {
+    gst_element_set_state(g_screencast.video_src_pipeline, GST_STATE_PAUSED);
+    printf("Disconnect with browser.");
+  }
+}
+
+static void on_icecandidate(char *sdp, void *data) {
+
+  signaling_send_answer_to_channel(g_screencast.signaling, sdp);
+  g_cond_signal(&g_screencast.cond);
+}
+
+static void on_transport_ready(void *data) {
+
+  gst_element_set_state(g_screencast.video_src_pipeline, GST_STATE_PLAYING);
+}
+
+static void need_data(GstElement *src, guint size, void *data) {
+ // printf("need data\n");
+}
+
+void on_track(uint8_t *packet, size_t bytes, void *data) {
+
+  GstBuffer *buf;
+  GstMapInfo info;
+
+  GstFlowReturn *ret;
+  buf = gst_buffer_new_and_alloc(bytes);
+
+  gst_buffer_map(buf, &info, GST_MAP_WRITE);
+
+  memcpy(info.data, packet, bytes);
+  info.size = bytes;
+
+  g_signal_emit_by_name(g_screencast.video_src, "push-buffer", buf, &ret);
+
+  gst_buffer_unmap(buf, &info);
+  gst_buffer_unref(buf);
+
+}
+
+void on_channel_event(SignalingEvent signaling_event, char *msg, void *data) {
+
+  if(signaling_event == SIGNALING_EVENT_GET_OFFER) {
+
+    printf("Get offer from singaling\n");
+    g_mutex_lock(&g_screencast.mutex);
+
+    if(g_screencast.pc)
+      peer_connection_destroy(g_screencast.pc);
+
+    g_screencast.pc = peer_connection_create();
+
+    MediaStream *media_stream = media_stream_new();
+    media_stream_add_track(media_stream, CODEC_H264);
+
+    peer_connection_add_stream(g_screencast.pc, media_stream);
+    Transceiver transceiver = {.video = RECVONLY};
+    peer_connection_add_transceiver(g_screencast.pc, transceiver);
+
+    peer_connection_ontrack(g_screencast.pc, on_track, NULL);
+    peer_connection_onicecandidate(g_screencast.pc, on_icecandidate, NULL);
+    peer_connection_oniceconnectionstatechange(g_screencast.pc, &on_iceconnectionstatechange, NULL);
+    peer_connection_set_on_transport_ready(g_screencast.pc, &on_transport_ready, NULL);
+    peer_connection_create_answer(g_screencast.pc);
+
+    g_cond_wait(&g_screencast.cond, &g_screencast.mutex);
+    peer_connection_set_remote_description(g_screencast.pc, msg);
+    g_mutex_unlock(&g_screencast.mutex);
+  }
+
+}
+
+
+void signal_handler(int signal) {
+
+  if(g_screencast.signaling)
+    signaling_destroy(g_screencast.signaling);
+
+  if(g_screencast.pc)
+    peer_connection_destroy(g_screencast.pc);
+
+  exit(0);
+}
+
+int main(int argc, char *argv[]) {
+
+  signal(SIGINT, signal_handler);
+
+  gst_init(&argc, &argv);
+
+  g_screencast.video_src_pipeline = gst_parse_launch(VIDEO_SRC_PIPELINE, NULL);
+
+  g_screencast.video_src = gst_bin_get_by_name(GST_BIN(g_screencast.video_src_pipeline), "video-app-src");
+
+  g_signal_connect(g_screencast.video_src, "need-data", G_CALLBACK(need_data), NULL);
+
+  g_object_set(g_screencast.video_src, "emit-signals", TRUE, NULL);
+
+  SignalingOption signaling_option = {SIGNALING_PROTOCOL_HTTP, "0.0.0.0", "demo", 8000, index_html};
+
+  g_screencast.signaling = signaling_create(signaling_option);
+  if(!g_screencast.signaling) {
+    printf("Create signaling service failed\n");
+    return 0;
+  }
+
+  signaling_on_channel_event(g_screencast.signaling, &on_channel_event, NULL);
+  signaling_dispatch(g_screencast.signaling);
+
+  return 0;
+}
+
