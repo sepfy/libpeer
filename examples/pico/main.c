9b32da306fb8284982631ed65186b7727948093e
sepfy <sepfy95@gmail.com>
2024-11-10 22:41:23 +0800

    [pico] Support i2s mic

--- examples/pico/main.c
+++ examples/pico/main.c
@@ -11,7 +11,13 @@
 #include "pico/stdlib.h"
 #include "task.h"
 
+#include "hardware/dma.h"
+
+#include "pcm-g711/pcm-g711/g711.h"
+#include "rp2040_i2s_example/i2s.h"
 #include "peer.h"
+
+static __attribute__((aligned(8))) pio_i2s i2s;
 
 #define TEST_TASK_PRIORITY (tskIDLE_PRIORITY + 1UL)
 
@@ -38,6 +44,46 @@ void onopen(void* userdata) {
 
 static void onclose(void* userdata) {
 }
+#if 1
+uint32_t get_epoch_time() {
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  return (uint32_t)tv.tv_sec * 1000 + tv.tv_usec / 1000;
+}
+#endif
+static void dma_i2s_in_handler(void) {
+
+  int8_t alaw[AUDIO_BUFFER_FRAMES];
+  int16_t pcm[AUDIO_BUFFER_FRAMES];
+  int32_t* input_buffer;
+  if (*(int32_t**)dma_hw->ch[i2s.dma_ch_in_ctrl].read_addr == i2s.input_buffer) {
+    input_buffer = i2s.input_buffer;
+  } else {
+    input_buffer = i2s.input_buffer + STEREO_BUFFER_SIZE;
+  }
+  for (int i = 0; i < AUDIO_BUFFER_FRAMES; i++) {
+    pcm[i] = (int16_t)(input_buffer[2*i+1] >> 16);
+    alaw[i] = ALaw_Encode(pcm[i]);
+  }
+
+#if 1
+static uint32_t total_bytes = 0;
+static uint32_t last_time = 0;
+total_bytes += AUDIO_BUFFER_FRAMES;
+uint32_t current_time = get_epoch_time();
+if (current_time - last_time > 1000) {
+printf("AUDIO_BUFFER_FRAMES: %d, bps: %d\n", AUDIO_BUFFER_FRAMES, 1000*total_bytes * 8 / (current_time - last_time));
+total_bytes = 0;
+last_time = current_time;
+}
+#endif
+
+  if (eState == PEER_CONNECTION_COMPLETED) {
+    peer_connection_send_audio(g_pc, alaw, AUDIO_BUFFER_FRAMES);
+  }
+
+  dma_hw->ints0 = 1u << i2s.dma_ch_in_data;  // clear the IRQ
+}
 
 void peer_connection_task() {
   printf("Run peer connection task on the core: %d\n", portGET_CORE_ID());
@@ -48,6 +94,7 @@ void peer_connection_task() {
 }
 
 void main_task(__unused void* params) {
+
   if (cyw43_arch_init()) {
     printf("failed to initialise\n");
     vTaskDelete(NULL);
@@ -68,6 +115,7 @@ void main_task(__unused void* params) {
   PeerConfiguration config = {
       .ice_servers = {
           {.urls = "stun:stun.l.google.com:19302"}},
+      .audio_codec = CODEC_PCMA,
       .datachannel = DATA_CHANNEL_STRING,
   };
 
@@ -83,6 +131,8 @@ void main_task(__unused void* params) {
   peer_signaling_join_channel();
 
   xTaskCreate(peer_connection_task, "PeerConnectionTask", 4096, NULL, TEST_TASK_PRIORITY, &xPcTaskHandle);
+
+  i2s_program_start_synched(pio0, &i2s_config_default, dma_i2s_in_handler, &i2s);
 
   printf("Run main task on the core: %d\n", portGET_CORE_ID());
   printf("open https://sepfy.github.io/webrtc?deviceId=mypico\n");
@@ -102,8 +152,9 @@ void vLaunch(void) {
 }
 
 int main(void) {
+
   stdio_init_all();
-  // set_sys_clock_khz(200000, true);
+  //set_sys_clock_khz(132000, true);
   vLaunch();
   return 0;
 }
